bfd43c5e5da84409f438d37965bcf427748c3a0d
xenjke <Petrenko.stas1@gmail.com>
2024-01-01 14:01:51 +0200

    remove `dotenv`; add `requirements.txt`

--- bot.py
+++ bot.py
@@ -1,28 +1,21 @@
 from telegram import Bot
 import asyncio
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-__chat_id = os.environ.get("DP_TARGET_CHAT_ID")
-__bot_token = os.environ.get("DP_BOT_ACCESS_TOKEN")
 
 
-async def __send_holiday(holiday):
+async def __send_holiday(holiday, bot_token, chat_id):
     from mappers import to_pretty_text_message
-    bot = Bot(token=__bot_token)
-    await bot.send_photo(chat_id=__chat_id,
+    bot = Bot(token=bot_token)
+    await bot.send_photo(chat_id=chat_id,
                          photo=holiday['image'],
                          caption=to_pretty_text_message(holiday), parse_mode='HTML')
     await asyncio.sleep(5)
 
 
-def send_holiday(holiday):
+def send_holiday(holiday, bot_token, chat_id):
     print(holiday)
     print('Sending...')
     try:
-        asyncio.run(__send_holiday(holiday))
-    except:
-        print('Failed!')
+        asyncio.run(__send_holiday(holiday, bot_token, chat_id))
+    except Exception as err:
+        print(F'Failed! {str(err)}')
     print('--------------------------------------------------------')
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import scrapper
 from bot import send_holiday
+import os
 
-for holiday in scrapper.get_holidays():
-    send_holiday(holiday)
+CHAT_ID = os.environ.get("DP_TARGET_CHAT_ID")
+BOT_TOKEN = os.environ.get("DP_BOT_ACCESS_TOKEN")
+
+
+def post_holidays():
+    for holiday in scrapper.get_holidays():
+        send_holiday(holiday, BOT_TOKEN, CHAT_ID)
+
+
+if __name__ == "__main__":
+    print('job started')
+    post_holidays()
+    print('job finished')
